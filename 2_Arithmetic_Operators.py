105a8926807cea550b4212ccaa993939809f44eb
thasneem <thasneem9mt@gmail.com>
2023-08-24 17:45:57 +0530

    add praction master

---
+++ 2_Arithmetic_Operators.py
@@ -0,0 +1,19 @@
+'''
+Task
+The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+The first line contains the sum of the two numbers.
+The second line contains the difference of the two numbers (first - second).
+The third line contains the product of the two numbers.
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    s = a+b
+    m = a-b
+    mal = a*b
+    #print(s,"\n",m,"\n",mal) 
+    print(s) 
+    print(m) 
+    print(mal) 
