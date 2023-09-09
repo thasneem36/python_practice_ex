5b14f8fbf105c8e40ae8b6d0f6d2760dbe3d3a15
thasneem36 <thasneem9mt@gmail.com>
2023-09-09 23:54:21 +0530

    update a red

--- red_8_FizzBuzz.py
+++ red_8_FizzBuzz.py
@@ -1,23 +1,43 @@
 
+# def fizzBuzz(n):
+#     # Write your code here
+    
+#     data3 = []
+#     data5 = []
+#     for i in range(n):
+#         i3 = i*3
+#         data3.append(i3)
+#         i5 = i*5
+#         data5.append(i5)
+    
+#     for i in range(1, n+1):
+        
+#         if i in data3:
+#             print(f'{i} Fizz')
+#         elif i in data5:
+#             print(f'{i} Buzz')
+#         else:
+#             print(i)
+    
+
+# if __name__ == '__main__':
+#     n = 15 #int(input().strip())
+
+#     fizzBuzz(n)
 
 def fizzBuzz(n):
-    # Write your code here
-    
-    for i in range(1,n+1):
-        i3 = i+3
-        if i == n:
-            print('FizzBuzz')
-        elif i in [3,6,9,12,15]:
-            print('Fizz')
-        elif i in [5,10,15]:
-            print('Buzz')
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print(f'{i} FizzBuzz')
+        elif i % 3 == 0:
+            print(f'{i} Fizz')
+        elif i % 5 == 0:
+            print(f'{i} Buzz')
         else:
             print(i)
-            
-        
-    
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    n = int(input()).strip()
 
     fizzBuzz(n)
+
